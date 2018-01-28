0d35f9cb3e92dde4eec98fb218b9bd941487fe5c
Yestli <yestli.team@gmail.com>
2018-01-28 16:06:25 -0600

    Código actualizado e integración con LCD

--- Arduino-UNO/Yestli-0.01U/Yestli-0.01U.ino
+++ Arduino-UNO/Yestli-0.01U/Yestli-0.01U.ino
@@ -1,10 +1,13 @@
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
-
-
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+PulseOximeter pox;
 void setup() {
-
-
+  lcd.begin(16, 2);
+  Serial.begin(115200);
+  
 }
 
 void loop() {
